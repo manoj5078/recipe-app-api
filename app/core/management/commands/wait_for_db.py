f7dfe641eb042e427bbee08dc559245cf45db507
manoj5078 <manojtolagekar@gmail.com>
2022-07-19 15:50:06 +0530

    updates

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,6 +8,8 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
+
+    
     '''Django command to wait for database. '''
     def handle(self, *args, **options):
         '''Entrypoint for command.'''
